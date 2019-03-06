f3a99549375f51e45142edbbb6772029d950e608
Artem Pivrik <artempivrik90@gmail.com>
2019-03-06 16:03:07 +0200

    add types in ts

--- linkedList.ts
+++ linkedList.ts
@@ -1,27 +1,78 @@
-//add(T, int index), remove(index), get(int index), size, equals, hashcode, toString
-class LinkedList {
-    constructor(value) {
+// type Primitive =string|number|null|boolean|undefined
+// class Stack<T> {
+//     items: T[];
+//
+//     constructor() {
+//         this.items = [];
+//     }
+//
+//     push(element: T): void {
+//         this.items.push(element);
+//     }
+//
+//     pop(): void | string {
+//         if (this.items.length == 0) {
+//             return "empty stack";
+//         } else this.items.pop();
+//     }
+//
+//     size():number {
+//         return this.items.length;
+//     }
+//
+//     toString(): string {
+//         return this.items.toString();
+//     }
+//
+//     equals(obj: T[]): boolean {
+//         return obj.toString() === this.items.toString();
+//
+//     }
+//
+// }
+//
+// let stackNumber = new Stack < Primitive >();
+// stackNumber.push(1);
+// stackNumber.push(2);
+// stackNumber.push(3);
+// stackNumber.push(3);
+// stackNumber.push('r');
+//
+// let a:boolean=stackNumber.equals(['e',2,3]);
+// let b:string=stackNumber.toString();
+// console.log(b);
+
+
+//add(T, int index), remove(index), get(int index), size, equals, toString
+class LinkedList<T> {
+    private head: { [key: string]: T };
+    private length: number;
+
+// newNode:{[key:string] : T}
+    constructor(value: T) {
         this.head = null;
         this.length = 0;
         this.addToHead(value);
     }
 
-    addToHead(value) {
-        const newNode = { value };
+    addToHead(value: T) {
+        let next: { [key: string]: T };
+        let newNode = {value,next};
         newNode.next = this.head;
         this.head = newNode;
         this.length++;
         return this;
     }
-    toString(){
-        var returnValue = String(this.head);
+
+    toString():string {
+        let returnValue = String(this.head);
         if (this.get().next) {
             returnValue = returnValue + "-> " + String(this.next);
         }
         return returnValue;
     }
 
-    _removeFromHead() {
+   private _removeFromHead() {
         if (this.length === 0) {
             return undefined;
         }
@@ -32,14 +83,16 @@ class LinkedList {
 
         return value;
     }
+
     size() {
         return this.length;
     }
+
     get(val) {
         let thisNode = this.head;
 
-        while(thisNode) {
-            if(thisNode.value === val) {
+        while (thisNode) {
+            if (thisNode.value === val) {
                 return thisNode;
             }
 
@@ -50,7 +103,7 @@ class LinkedList {
     }
 
     remove(val) {
-        if(this.length === 0) {
+        if (this.length === 0) {
             return undefined;
         }
 
@@ -61,8 +114,8 @@ class LinkedList {
         let previousNode = this.head;
         let thisNode = previousNode.next;
 
-        while(thisNode) {
-            if(thisNode.value === val) {
+        while (thisNode) {
+            if (thisNode.value === val) {
                 break;
             }
 
@@ -81,10 +134,10 @@ class LinkedList {
 
 }
 
-const list =new LinkedList('first');
+const list = new LinkedList('first');
 list.addToHead('second');
 list.addToHead('third');
 // list.remove('second');
-const s =list.get('second');
+const s = list.get('second');
 
 console.log(s);
