c0882bb10bdff5660cbdd0ebcc428a04b69e2d51
Artem Pivrik <artempivrik90@gmail.com>
2019-03-06 02:52:33 +0200

    add types to class

--- stack.ts
+++ stack.ts
@@ -1,36 +1,51 @@
-class Stack {
+type Primitive =string|number|null|boolean|undefined
+class Stack<T> {
+    items: T[];
 
     constructor() {
         this.items = [];
     }
 
-    push(element) {
+    push(element: T): void {
         this.items.push(element);
     }
 
-    pop() {
+    pop(): void | string {
         if (this.items.length == 0) {
             return "empty stack";
         } else this.items.pop();
     }
 
-    size() {
-        return this.items;
+    size():number {
+        return this.items.length;
     }
 
-    toString() {
+    toString(): string {
         return this.items.toString();
     }
 
-    hashcode() {
-        const str = this.toString();
-        return Array.from(str)
-            .reduce((s, c) => Math.imul(31, s) + c.charCodeAt(0) | 0, 0)
+    // hashcode():number {
+    //     const str:string = this.toString();
+    //     return Array.from(str)
+    //         .reduce((s, c) => Math.imul(31, s) + c.charCodeAt(0) | 0, 0)
+    // }
+
+    equals(obj: T[]): boolean {
+        return obj.toString() === this.items.toString();
+
     }
 
-    equals(obj) {
-               return obj.toString() === this.items.toString();
-
-    }
 }
+
+let stackNumber = new Stack < Primitive >();
+stackNumber.push(1);
+stackNumber.push(2);
+stackNumber.push(3);
+stackNumber.push(3);
+stackNumber.push('r');
+
+let a:boolean=stackNumber.equals(['e',2,3]);
+let b:string=stackNumber.toString();
+console.log(b);
+
 
